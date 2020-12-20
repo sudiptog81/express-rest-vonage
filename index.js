0e17f4624cc8fb9e321af653ca1c8e0d2f39c268
Sudipto Ghosh <sudipto@ghosh.pro>
2020-12-20 12:29:08 +0530

    added precommit hooks for linting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const fs = require('fs');
+const fs = require("fs");
 
-const express = require('express');
-const Vonage = require('@vonage/server-sdk');
+const express = require("express");
+const Vonage = require("@vonage/server-sdk");
 
 const PORT = process.env.PORT || 3000;
 const {
@@ -11,7 +11,7 @@ const {
   VONAGE_API_KEY,
   VONAGE_API_SECRET,
   VONAGE_APPLICATION_ID,
-  VONAGE_PRIVATE_KEY_PATH
+  VONAGE_PRIVATE_KEY_PATH,
 } = process.env;
 
 const app = express();
@@ -19,60 +19,63 @@ const client = new Vonage({
   apiKey: VONAGE_API_KEY,
   apiSecret: VONAGE_API_SECRET,
   applicationId: VONAGE_APPLICATION_ID,
-  privateKey: fs.readFileSync(VONAGE_PRIVATE_KEY_PATH)
+  privateKey: fs.readFileSync(VONAGE_PRIVATE_KEY_PATH),
 });
 
 app.use(express.json());
 
 app
-  .post('/insights', (req, res) => {
-    client.numberInsight.get({
-      level: 'standard',
-      number: req.body.number
-    }, (error, response) => {
-      if (error)
-        console.error(error);
-      if (response)
-        console.log(response);
-    });
-    res.sendStatus(204);
-  })
-  .post('/sms', (req, res) => {
-    client
-      .message
-      .sendSms(
-        VONAGE_NUMBER,
-        req.body.to,
-        req.body.text,
-        {},
-        (error, response) => {
-          if (error)
-            console.error(error);
-          if (response)
-            console.log(response);
-        });
-    res.sendStatus(204);
-  })
-  .post('/voice', (req, res) => {
-    client.calls.create({
-      to: [{
-        type: 'phone',
-        number: req.body.to
-      }],
-      from: {
-        type: 'phone',
-        number: VONAGE_NUMBER
+  .post("/insights", (req, res) => {
+    client.numberInsight.get(
+      {
+        level: "standard",
+        number: req.body.number,
       },
-      ncco: [{
-        action: 'talk',
-        text: 'This is a text to speech call from Vonage.'
-      }]
-    }, (error, response) => {
-      if (error)
-        console.error(error);
-      if (response)
-        console.log(response);
-    });
+      (error, response) => {
+        if (error) console.error(error);
+        if (response) console.log(response);
+      }
+    );
+    res.sendStatus(204);
+  })
+  .post("/sms", (req, res) => {
+    client.message.sendSms(
+      VONAGE_NUMBER,
+      req.body.to,
+      req.body.text,
+      {},
+      (error, response) => {
+        if (error) console.error(error);
+        if (response) console.log(response);
+      }
+    );
+    res.sendStatus(204);
+  })
+  .post("/voice", (req, res) => {
+    client.calls.create(
+      {
+        to: [
+          {
+            type: "phone",
+            number: req.body.to,
+          },
+        ],
+        from: {
+          type: "phone",
+          number: VONAGE_NUMBER,
+        },
+        ncco: [
+          {
+            action: "talk",
+            text: "This is a text to speech call from Vonage.",
+          },
+        ],
+      },
+      (error, response) => {
+        if (error) console.error(error);
+        if (response) console.log(response);
+      }
+    );
     res.sendStatus(204);
   });
 
